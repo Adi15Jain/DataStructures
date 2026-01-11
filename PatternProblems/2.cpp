347aad5a03090536ad496094367393d8d07c77a3
Adi Jain <adi1510jain@gmail.com>
2026-01-11 23:43:31 +0530

    Pattern Problems

---
+++ PatternProblems/2.cpp
@@ -0,0 +1,20 @@
+// 2. To print a number increasing triangle.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n;
+    cout << "Enter the value upto which pattern is to be printed: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
