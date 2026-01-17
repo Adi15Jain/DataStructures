4500019905d80162fa01e98cfacac170750e3af0
Adi Jain <adi1510jain@gmail.com>
2026-01-17 23:49:14 +0530

    More pattern problems

---
+++ PatternProblems/9.cpp
@@ -0,0 +1,23 @@
+// 9. To print a hollow square pattern.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter the size of hollow square: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == 1 || i == n || j == 1 || j == n) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
