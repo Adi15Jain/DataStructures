71a2e68dce63dacae66ae5284bb9e3df32672b5f
Adi Jain <adi1510jain@gmail.com>
2026-02-18 23:06:48 +0530

    Updated variables

--- PatternProblems/4.cpp
+++ PatternProblems/4.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main () {
-    int n;
+    int x;
     cout << "Enter number upto which triangle is to be made: ";
-    cin >> n;
+    cin >> x;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= x; i++) {
         for (int j = 1; j <= i; j++) {
             cout << i;
         }
